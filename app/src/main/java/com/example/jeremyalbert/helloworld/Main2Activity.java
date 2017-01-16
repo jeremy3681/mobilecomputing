66a7c47875e9a01f9a9c302ed09e44ff0690ba82
Jeremy Albert <jeremy3681@gmail.com>
2017-01-15 16:35:20 -0800

    Different Pages

--- app/src/main/java/com/example/jeremyalbert/helloworld/Main2Activity.java
+++ app/src/main/java/com/example/jeremyalbert/helloworld/Main2Activity.java
@@ -4,18 +4,17 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class Main2Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main2);
     }
-    public void nextPage(View view)
+    public void prevPage(View view)
     {
-        Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+        Intent intent = new Intent(Main2Activity.this,MainActivity.class);
         startActivity(intent);
     }
 }
